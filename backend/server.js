1ea49bec574ac88f94ac50d2fd9580a4ddf4e99b
shivang dixit <shivangtg857@gmail.com>
2025-01-15 09:28:51 +0530

    backend changes

--- backend/server.js
+++ backend/server.js
@@ -7,25 +7,31 @@ const isLoggedIn = require("./middleware/isLoggedIn");
 const userRouter = require("./routes/userRoutes");
 const app=express();
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5173/signup", "http://localhost:5173/login","http://localhost:5173/update","https://course-helper-beta.vercel.app"];
-
-app.use(express.json());
-app.use((req, res, next) => {
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://course-helper-beta.vercel.app",
+    "http://localhost:5173/",
+    "https://course-helper-beta.vercel.app/"
+  ];
+  
+  app.use(express.json());
+  
+  app.use((req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
-        res.header("Access-Control-Allow-Origin", origin);
+      res.header("Access-Control-Allow-Origin", origin);
+      res.header("Access-Control-Allow-Credentials", "true");
     }
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    // res.header("Content-Security-Policy", "default-src 'self'")
-    // Handle preflight requests
-    if (req.method === "OPTIONS") {
-      return res.sendStatus(204); // No Content
-    }
     
+    if (req.method === "OPTIONS") {
+      return res.sendStatus(204); // No Content for preflight requests
+    }
+  
     next();
-});
-
+  });
+  
 app.use("/api/card/",isLoggedIn,courseRouter);
 app.use("/api/user",userRouter)
 
